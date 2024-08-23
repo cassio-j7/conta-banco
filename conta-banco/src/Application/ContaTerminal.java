a0836e45b7aa486d6f51774b331fa2773a5f007b
cassio-j7 <engestr4@gmail.com>
2024-08-23 11:33:27 -0300

    Meu projeto conta banco

---
+++ conta-banco/src/Application/ContaTerminal.java
@@ -0,0 +1,12 @@
+package Application;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Wold!!");
+
+	}
+
+}
